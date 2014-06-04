b182a66ec2f3976d1a5d359ec4526a7a45626ba2
opensoftlutions <luisgil@opensoftlutions.com>
2014-06-04 13:37:56 +0200

    Curso c#
    Estructuras repetitivas

---
+++ EstructurasRepetitivas/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasRepetitivas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Vamos a imprimir los 4 primeros numeros
+
+            for (int i = 1; i < 5; i++) {
+                Console.WriteLine("Va por el numero {0}", i);
+            
+            }
+
+            int numero=1;
+            while (numero < 5) {
+
+                Console.WriteLine("Va por el numero {0}", numero);
+                numero++;
+            }
+
+            numero = 1;
+
+            do
+            {
+                Console.WriteLine("Va por el numero {0}", numero);
+                numero++;
+            } while (numero < 5);
+
+
+            //Si lo que queremos es ver el contenido de un array
+
+            String[] nombres = { "Eva", "Juan", "Pedro", "Antonio" };
+
+            foreach (var nombre in nombres) {
+
+                Console.WriteLine("El nombre es {0}", nombre);
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
